7f3959fa6a08f495ff2ff4ea8b5bb8025062eeca
Shannon Du <longingdxl@gmail.com>
2020-09-03 10:09:21 +0800

    [ASF] Initial draft of ASF implementation

---
+++ MdePkg/Include/Asf.h
@@ -0,0 +1,183 @@
+#ifndef _AMD_ASF_H_
+#define _AMD_ASF_H_
+
+#include <Base.h>
+// #include <Ppi/ReportStatusCodeHandler.h>
+#include <Pi/PiStatusCode.h>
+
+#define ASF_BIOS_WATCHDOG_TIMER           0       // Platform firmware
+#define ASF_OS_WATCHDOG_TIMER             0x48    // OS vendor         
+#define START_TIMER                       TRUE
+#define STOP_TIMER                        FALSE
+
+#define EVENT_TYPE_PROGRESS               0
+#define EVENT_TYPE_ERROR                  1
+#define EVENT_TYPE_SYSTEM_MANAGEMENT      2
+
+#define ASFMSG_CMD_SENSOR_DEV_SYSSTATE    1	 // Sensor Device and System State
+#define ASFMSG_SUBCMD_GET_EVENT_DATA	    0x12 // Get Event Data
+#define ASFMSG_SUBCMD_GET_EVENT_STS		    0x12 // Get Event Status
+#define ASFMSG_SUBCMD_DEV_TYPE_POLL		    0x13 // Device Type Poll
+#define ASFMSG_SUBCMD_SET_SYS_STATE		    0x18 // Set System State(00010100??))
+
+#define ASFMSG_CMD_MGM_CTRL               2	 // Management Control
+#define ASFMSG_SUBCMD_START_WD_TIMER	    0x13 // Start Watchdog Timer
+#define ASFMSG_SUBCMD_STOP_WD_TIMER		    0x14 // Stop Watchdog Timer
+
+#define ASFMSG_CMD_CONFIG                 3	 // ASF Configuration
+#define ASFMSG_SUBCMD_CLR_BOOT_OPTION     0x15 // Clear Boot Options
+#define ASFMSG_SUBCMD_RET_BOOT_OPTION	    0x16 // Return Boot Options
+#define ASFMSG_SUBCMD_NO_BOOT_OPTION	    0x17 // No Boot Options
+
+#define ASFMSG_CMD_MSG                    4	 // ASF Messaging
+#define ASFMSG_SUBCMD_MSG_RETRANSMIT	    0x15 // Push Message with Retransmission
+#define ASFMSG_SUBCMD_MSG_NORETRANSMIT	  0x16 // Push Message without Retransmission
+#define ASFMSG_SUBCMD_SET_ALRT_CFG		    0x11 // Set Alert Configuration
+#define ASFMSG_SUBCMD_GET_ALRT_CFG		    0x12 // Get Alert Configuration
+
+#define ASFMSG_SYSTEM_STATE_S0		        0 // S0/G0 "Working"
+#define ASFMSG_SYSTEM_STATE_S1		        1 // S1
+#define ASFMSG_SYSTEM_STATE_S2		        2 // S2
+#define ASFMSG_SYSTEM_STATE_S3		        3 // S3
+#define ASFMSG_SYSTEM_STATE_S4		        4 // S4
+#define ASFMSG_SYSTEM_STATE_S5		        5 // S5/G2 "Soft-off"
+
+//Asf Boot Option special cmd
+#define ASF_SCMD_NOP                        0  
+#define ASF_SCMD_FORCE_PXE_BOOT             1
+#define ASF_SCMD_FORCE_HARD_DRIVE_BOOT      2
+#define ASF_SCMD_FORCE_HARD_DRIVE_SAFE_BOOT 3
+#define ASF_SCMD_FORCE_DIAGNOSTIC_BOOT      4
+#define ASF_SCMD_FORCE_CD_BOOT              5
+
+//Asf Boot Option Bit
+#define ASF_BOP_BIT_LOCK_POWER_BUTTON           0x02    //BIT1
+#define ASF_BOP_BIT_LOCK_RESET_BUTTON           0x04    //BIT2
+#define ASF_BOP_BIT_LOCK_KEYBOARD               0x20    //BIT5
+#define ASF_BOP_BIT_LOCK_SLEEP_BUTTON           0x40    //BIT6
+#define ASF_BOP_BIT_PASSWORD_BYPASS             0x800  //BIT11
+#define ASF_BOP_BIT_FORCE_PROGRESS_EVENT        0x1000  //BIT12
+#define ASF_BOP_BIT_FIRMWARE_VERBOSITY          0x6000  //BIT13+BIT14
+#define ASF_BOP_BIT_CONFIGURATION_DATA_RESET    0x8000  //BIT15
+
+/**
+  This is the ASF Boot Option data structure.
+
+**/
+typedef struct _ASF_BOOT_OPTION
+{
+	UINT8       SubCommand;
+	UINT8       VersionNumber;
+	UINT32      IANAId;
+	UINT8       SpecialCmd;
+	UINT16      SpecCmdParam;
+	UINT16      BootOptionBit;
+	UINT16      OEMParam;
+} ASF_BOOT_OPTION;
+
+typedef struct _ASF_MSG_NORETRANSMIT {
+  UINT8       Command;
+  UINT8       Length;
+  UINT8       SubCommand;
+  UINT8       VersionNumber;	
+  UINT8       EventSensorType;	
+  UINT8       EventType;	
+  UINT8       EventOffset;	
+  UINT8       EventSourceType;	
+  UINT8       EventSeverity;
+  UINT8       SensorDevice;	
+  UINT8       SensorNumber;	
+  UINT8       Entity;
+  UINT8       EntityInstance;
+  UINT8       EventData1;
+  UINT8       EventData2;
+  UINT8       EventData3;
+  UINT8       EventData4;
+  UINT8       EventData5;
+  UINT8       EventData6;
+} ASF_MSG_NORETRANSMIT;
+
+
+typedef struct _ASF_START_WATCHDOG {
+	UINT8       Command;
+	UINT8       Length;
+	UINT8       SubCommand;
+	UINT8       VersionNumber;
+  UINT16      TimeoutValue;
+	UINT8       EventSensorType;
+	UINT8       EventType;
+	UINT8       EventOffset;
+	UINT8       EventSourceType;
+	UINT8       EventSeverity;
+	UINT8       SensorDevice;
+	UINT8       SensorNumber;
+	UINT8       Entity;
+	UINT8       EntityInstance;
+  UINT8       EventData1;
+  UINT8       EventData2;
+  UINT8       EventData3;
+  UINT8       EventData4;
+  UINT8       EventData5;
+  UINT8       EventData6;
+} ASF_START_WATCHDOG;
+
+/**
+  This is the ASF START WatchDog Timer Data structure.
+**/
+typedef struct _ASF_STOP_WATCHDOG
+{
+	UINT8       Command;
+	UINT8       Length;
+	UINT8       SubCommand;
+	UINT8       VersionNumber;
+} ASF_STOP_WATCHDOG;
+
+/**
+  This is the ASF Message Type structure.
+**/
+typedef enum {
+  MsgHddInit, 
+  MsgApInit,
+  MsgUserInitSetup,   
+  MsgUsbResourceConfig,
+  MsgPciResourceConfig,
+  MsgVideoInit,   
+  MsgKbcInit, 
+  MsgKbcTest, 
+  MsgOSBoot,  
+  MsgMotherBoardInit, 
+  MsgNoVideo,  
+  MsgKbdFailure,
+  MsgHddFailure, 
+  MsgChassisIntrusion,
+  MsgUserPasswordInvalid,
+  MsgNoBootMedia,
+  MsgSystemStateS0,
+  MsgSystemStateS3,
+  MsgSystemStateS4,
+  MsgSystemStateS5,
+  MsgStartBiosWatchdogTimer,
+  MsgStopBiosWatchdogTimer,
+  MsgStartOsWatchdogTimer,
+  MsgStopOsWatchdogtimer
+} ASF_MESSAGE_TYPE;
+
+/**
+  This is the Message Data Hub Map Structure.
+**/
+typedef struct {
+  ASF_MESSAGE_TYPE            MessageType;
+  EFI_STATUS_CODE_VALUE       StatusCode;
+} MESSAGE_DATA_HUB_MAP;
+
+/**
+  This is the ASF System Firmware Event Structure.
+**/
+typedef struct {
+  ASF_MESSAGE_TYPE            Type;
+  ASF_MSG_NORETRANSMIT        Message;
+  EFI_STATUS_CODE_VALUE       StatusCode;
+  UINT16                      MessageString[128];
+} ASF_MESSAGE;
+
+#endif
---
+++ OvmfPkg/Asf/AsfDxe/AsfDxe.h
@@ -0,0 +1,523 @@
+#ifndef _ASF_DXE_H_
+#define _ASF_DXE_H_
+
+
+// #include <Protocol/DataHub.h>
+#include <Protocol/ReportStatusCodeHandler.h>
+
+#include <Library/BaseLib.h>
+#include <Library/UefiBootServicesTableLib.h>
+#include <Library/UefiRuntimeServicesTableLib.h>
+#include <Library/DebugLib.h>
+#include <Library/UefiLib.h>
+#include <Library/HobLib.h>
+#include <Library/BaseMemoryLib.h>
+#include <Guid/GlobalVariable.h>
+#include <Uefi/UefiBaseType.h>
+#include <Uefi/UefiSpec.h>
+#include <Pi/PiStatusCode.h>
+
+#include <Asf.h>
+
+#ifndef EFI_CU_CHIPSET_PC_INTRUDER_DETECT
+#define EFI_CU_CHIPSET_PC_INTRUDER_DETECT   (EFI_SUBCLASS_SPECIFIC | 0x00000000)
+#endif
+
+#ifndef EFI_SW_DXE_BS_EC_NO_BOOT_MEDIA
+#define EFI_SW_DXE_BS_EC_NO_BOOT_MEDIA      (EFI_SUBCLASS_SPECIFIC | 0x00000001)
+#endif
+
+static const ASF_MESSAGE mAsfMsgHddInit = \
+{
+  MsgHddInit,
+  {
+    0x04,   // Command
+    0x0D,   // Length
+    0x16,   // SubCommand
+    0x10,   // VersionNumber
+    0x0F,   // EventSensorType
+    0x6F,   // EventType
+    0x02,   // EventOffset
+    0x68,   // EventSourceType
+    0x08,   // EventSeverity
+    0xFF,   // SensorDevice
+    0xFF,   // SensorNumber
+    0x04,   // Entity
+    0x00,   // EntityInstance
+    0x40,   // EventData1
+    0x02    // EventData2
+  },
+  (EFI_IO_BUS_ATA_ATAPI | EFI_IOB_PC_DETECT),
+  L"ASF - PROGRESS - HDD INIT"
+};
+
+static const ASF_MESSAGE mAsfMsgApInit = \
+{
+  MsgApInit,
+  {
+    0x04,   // Command
+    0x0D,   // Length
+    0x16,   // SubCommand
+    0x10,   // VersionNumber
+    0x0F,   // EventSensorType
+    0x6F,   // EventType
+    0x02,   // EventOffset
+    0x68,   // EventSourceType
+    0x08,   // EventSeverity
+    0xFF,   // SensorDevice
+    0xFF,   // SensorNumber
+    0x03,   // Entity
+    0x00,   // EntityInstance
+    0x40,   // EventData1
+    0x03    // EventData2
+  },
+  (EFI_COMPUTING_UNIT_HOST_PROCESSOR | EFI_CU_HP_PC_AP_INIT),
+  L"ASF - PROGRESS - AP Init"
+};
+
+static const ASF_MESSAGE mAsfMsgUserInitSetup = \
+{
+  MsgUserInitSetup,
+  {
+    0x04,   // Command
+    0x0D,   // Length
+    0x16,   // SubCommand
+    0x10,   // VersionNumber
+    0x0F,   // EventSensorType
+    0x6F,   // EventType
+    0x02,   // EventOffset
+    0x68,   // EventSourceType
+    0x08,   // EventSeverity
+    0xFF,   // SensorDevice
+    0xFF,   // SensorNumber
+    0x22,   // Entity
+    0x00,   // EntityInstance
+    0x40,   // EventData1
+    0x05    // EventData2
+  },
+  (EFI_SOFTWARE_DXE_BS_DRIVER | EFI_SW_PC_USER_SETUP),
+  L"ASF - PROGRESS - User Init Setup"
+};
+
+static const ASF_MESSAGE mAsfMsgUsbResouceConfig = \
+{
+  MsgUsbResourceConfig,
+  {
+    0x04,   // Command
+    0x0D,   // Length
+    0x16,   // SubCommand
+    0x10,   // VersionNumber
+    0x0F,   // EventSensorType
+    0x6F,   // EventType
+    0x02,   // EventOffset
+    0x68,   // EventSourceType
+    0x08,   // EventSeverity
+    0xFF,   // SensorDevice
+    0xFF,   // SensorNumber
+    0x22,   // Entity
+    0x00,   // EntityInstance
+    0x40,   // EventData1
+    0x06    // EventData2
+  },
+  (EFI_IO_BUS_USB | EFI_P_PC_ENABLE),
+  L"ASF - PROGRESS - USB Resource Config"
+};
+
+static const ASF_MESSAGE mAsfMsgPciResourceConfig = \
+{
+  MsgPciResourceConfig,
+  {
+    0x04,   // Command
+    0x0D,   // Length
+    0x16,   // SubCommand
+    0x10,   // VersionNumber
+    0x0F,   // EventSensorType
+    0x6F,   // EventType
+    0x02,   // EventOffset
+    0x68,   // EventSourceType
+    0x08,   // EventSeverity
+    0xFF,   // SensorDevice
+    0xFF,   // SensorNumber
+    0x22,   // Entity
+    0x00,   // EntityInstance
+    0x40,   // EventData1
+    0x07    // EventData2
+  },
+  (EFI_IO_BUS_PCI | EFI_IOB_PCI_BUS_ENUM),
+  L"ASF - PROGRESS - PCI Resource Config"
+};
+
+static const ASF_MESSAGE mAsfMsgVideoInit = \
+{
+  MsgVideoInit,
+  {
+    0x04,   // Command
+    0x0D,   // Length
+    0x16,   // SubCommand
+    0x10,   // VersionNumber
+    0x0F,   // EventSensorType
+    0x6F,   // EventType
+    0x02,   // EventOffset
+    0x68,   // EventSourceType
+    0x08,   // EventSeverity
+    0xFF,   // SensorDevice
+    0xFF,   // SensorNumber
+    0x0b,   // Entity
+    0x00,   // EntityInstance
+    0x40,   // EventData1
+    0x09    // EventData2
+  },
+  (EFI_PERIPHERAL_LOCAL_CONSOLE | EFI_P_PC_INIT),
+  L"ASF - PROGRESS - Video Init"
+};
+
+static const ASF_MESSAGE mAsfMsgKbcInit = \
+{
+  MsgKbcInit,
+  {
+    0x04,   // Command
+    0x0D,   // Length
+    0x16,   // SubCommand
+    0x10,   // VersionNumber
+    0x0F,   // EventSensorType
+    0x6F,   // EventType
+    0x02,   // EventOffset
+    0x68,   // EventSourceType
+    0x08,   // EventSeverity
+    0xFF,   // SensorDevice
+    0xFF,   // SensorNumber
+    0x07,   // Entity
+    0x00,   // EntityInstance
+    0x40,   // EventData1
+    0x0C    // EventData2
+  },
+  (EFI_PERIPHERAL_KEYBOARD | EFI_P_PC_INIT),
+  L"ASF - PROGRESS - KBC Init"
+};
+
+static const ASF_MESSAGE mAsfMsgKbcTest = \
+{
+  MsgKbcTest,
+  {
+    0x04,   // Command
+    0x0D,   // Length
+    0x16,   // SubCommand
+    0x10,   // VersionNumber
+    0x0F,   // EventSensorType
+    0x6F,   // EventType
+    0x02,   // EventOffset
+    0x68,   // EventSourceType
+    0x08,   // EventSeverity
+    0xFF,   // SensorDevice
+    0xFF,   // SensorNumber
+    0x07,   // Entity
+    0x00,   // EntityInstance
+    0x40,   // EventData1
+    0x17    // EventData2
+  },
+  (EFI_PERIPHERAL_KEYBOARD | EFI_P_KEYBOARD_PC_SELF_TEST),
+  L"ASF - PROGRESS - KBC Teset"
+};
+
+static const ASF_MESSAGE mAsfMsgOsBoot = \
+{
+  MsgOSBoot,
+  {
+    0x04,   // Command
+    0x0D,   // Length
+    0x16,   // SubCommand
+    0x10,   // VersionNumber
+    0x0F,   // EventSensorType
+    0x6F,   // EventType
+    0x02,   // EventOffset
+    0x68,   // EventSourceType
+    0x08,   // EventSeverity
+    0xFF,   // SensorDevice
+    0xFF,   // SensorNumber
+    0x00,   // Entity
+    0x00,   // EntityInstance
+    0x40,   // EventData1
+    0x13    // EventData2
+  },
+  (EFI_SOFTWARE_DXE_BS_DRIVER | EFI_SW_DXE_BS_PC_LEGACY_BOOT_EVENT),
+  L"ASF - PROGRESS - OS Boot"
+};
+
+static const ASF_MESSAGE mAsfMsgMotherBoardInit = \
+{
+  MsgMotherBoardInit,
+  {
+    0x04,   // Command
+    0x0D,   // Length
+    0x16,   // SubCommand
+    0x10,   // VersionNumber
+    0x0F,   // EventSensorType
+    0x6F,   // EventType
+    0x02,   // EventOffset
+    0x68,   // EventSourceType
+    0x01,   // EventSeverity
+    0xFF,   // SensorDevice
+    0xFF,   // SensorNumber
+    0x07,   // Entity
+    0x00,   // EntityInstance
+    0x40,   // EventData1
+    0x14    // EventData2
+  },
+  (EFI_COMPUTING_UNIT_CHIPSET | EFI_CU_PC_INIT_BEGIN),
+  L"ASF - PROGRESS - Mother Board Init"
+};
+
+static const ASF_MESSAGE mAsfMsgNoVideo = \
+{
+  MsgNoVideo,
+  {
+    0x04,   // Command
+    0x0D,   // Length
+    0x16,   // SubCommand
+    0x10,   // VersionNumber
+    0x0F,   // EventSensorType
+    0x6F,   // EventType
+    0x00,   // EventOffset
+    0x68,   // EventSourceType
+    0x08,   // EventSeverity
+    0xFF,   // SensorDevice
+    0xFF,   // SensorNumber
+    0x0B,   // Entity
+    0x00,   // EntityInstance
+    0x40,   // EventData1
+    0x0A    // EventData2
+  },
+  (EFI_PERIPHERAL_LOCAL_CONSOLE | EFI_P_EC_NOT_DETECTED),
+  L"ASF - ERROR - NO Vdieo"
+};
+
+static const ASF_MESSAGE mAsfMsgKbdFailure = \
+{
+  MsgKbdFailure,
+  {
+    0x04,   // Command
+    0x0D,   // Length
+    0x16,   // SubCommand
+    0x10,   // VersionNumber
+    0x0F,   // EventSensorType
+    0x6F,   // EventType
+    0x00,   // EventOffset
+    0x68,   // EventSourceType
+    0x08,   // EventSeverity
+    0xFF,   // SensorDevice
+    0xFF,   // SensorNumber
+    0x07,   // Entity
+    0x00,   // EntityInstance
+    0x40,   // EventData1
+    0x07    // EventData2
+  },
+  (EFI_PERIPHERAL_KEYBOARD | EFI_P_EC_NOT_DETECTED),
+  L"ASF - ERROR - Keyboard Failure"
+};
+
+static const ASF_MESSAGE mAsfMsgHddFailure = \
+{
+  MsgHddFailure,
+  {
+    0x04,   // Command
+    0x0D,   // Length
+    0x16,   // SubCommand
+    0x10,   // VersionNumber
+    0x0F,   // EventSensorType
+    0x6F,   // EventType
+    0x00,   // EventOffset
+    0x68,   // EventSourceType
+    0x20,   // EventSeverity
+    0xFF,   // SensorDevice
+    0xFF,   // SensorNumber
+    0x04,   // Entity
+    0x00,   // EntityInstance
+    0x40,   // EventData1
+    0x03    // EventData2
+  },
+  (EFI_PERIPHERAL_FIXED_MEDIA | EFI_P_EC_NOT_DETECTED),
+  L"ASF - ERROR - HDD Failure"
+};
+
+static const ASF_MESSAGE mAsfMsgChassisIntrusion = \
+{
+  MsgChassisIntrusion,
+  {
+    0x04,   // Command
+    0x0B,   // Length
+    0x16,   // SubCommand
+    0x10,   // VersionNumber
+    0x05,   // EventSensorType
+    0x6F,   // EventType
+    0x00,   // EventOffset
+    0x68,   // EventSourceType
+    0x01,   // EventSeverity
+    0xFF,   // SensorDevice
+    0xFF,   // SensorNumber
+    0x07,   // Entity
+    0x00    // EntityInstance
+  },
+  (EFI_COMPUTING_UNIT_CHIPSET | EFI_CU_CHIPSET_PC_INTRUDER_DETECT),
+  L"ASF - ERROR - Chassis Intrusion"
+};
+
+static const ASF_MESSAGE mAsfMsgUserPasswordInvalid = \
+{
+  MsgUserPasswordInvalid,
+  {
+    0x04,   // Command
+    0x0B,   // Length
+    0x16,   // SubCommand
+    0x10,   // VersionNumber
+    0x06,   // EventSensorType
+    0x6F,   // EventType
+    0x01,   // EventOffset
+    0x68,   // EventSourceType
+    0x08,   // EventSeverity
+    0xFF,   // SensorDevice
+    0xFF,   // SensorNumber
+    0x22,   // Entity
+    0x00    // EntityInstance
+  },
+  (EFI_SOFTWARE_DXE_BS_DRIVER | EFI_SW_DXE_BS_EC_INVALID_PASSWORD),
+  L"ASF - ERROR - User Password Invalid"
+};
+
+static const ASF_MESSAGE mAsfMsgNoBootMedia = \
+{
+  MsgNoBootMedia,
+  {
+    0x04,   // Command
+    0x0D,   // Length
+    0x16,   // SubCommand
+    0x10,   // VersionNumber
+    0x1E,   // EventSensorType
+    0x6F,   // EventType
+    0x00,   // EventOffset
+    0x68,   // EventSourceType
+    0x20,   // EventSeverity
+    0xFF,   // SensorDevice
+    0xFF,   // SensorNumber
+    0x00,   // Entity
+    0x00,   // EntityInstance
+    0x40,   // EventData1
+    0x00    // EventData2
+  },
+  (EFI_SOFTWARE_DXE_BS_DRIVER | EFI_SW_DXE_BS_EC_NO_BOOT_MEDIA),
+  L"ASF - ERROR - No Boot Media"
+};
+
+ASF_MESSAGE mAsfSystemStateS0 = \
+{
+  MsgSystemStateS0,
+  {
+    0x01,   // Command
+    0x03,   // Length
+    0x18,   // SubCommand
+    0x10,   // VersionNumber
+    0x00,   // SystemState
+  },
+  0,        // Not valid
+  L"ASF - MESSAGE - System State S0"
+};
+
+ASF_MESSAGE mAsfSystemStateS3 = \
+{
+  MsgSystemStateS3,
+  {
+    0x01,   // Command
+    0x03,   // Length
+    0x18,   // SubCommand
+    0x10,   // VersionNumber
+    0x03,   // SystemState
+  },
+  0,        // Not valid
+  L"ASF - MESSAGE - System State S3"
+};
+
+ASF_MESSAGE mAsfSystemStateS4 = \
+{
+  MsgSystemStateS4,
+  {
+    0x01,   // Command
+    0x03,   // Length
+    0x18,   // SubCommand
+    0x10,   // VersionNumber
+    0x04,   // SystemState
+  },
+  0,        // Not valid
+  L"ASF - MESSAGE - System State S4"
+};
+
+ASF_MESSAGE mAsfSystemStateS5 = \
+{
+  MsgSystemStateS5,
+  {
+    0x01,   // Command
+    0x03,   // Length
+    0x18,   // SubCommand
+    0x10,   // VersionNumber
+    0x05,   // SystemState
+  },
+  0,        // Not valid
+  L"ASF - MESSAGE - System State S5"
+};
+
+static const ASF_MESSAGE mAsfMsgStartBiosWatchdogTimer = \
+{
+  MsgStartBiosWatchdogTimer,
+  {
+    0x02,   // Command
+    0x0F,   // Length
+    0x16,   // SubCommand
+    0x10,   // VersionNumber
+    0x0F,   // EventSensorType
+    0x6F,   // EventType
+    0x00,   // EventOffset
+    0x68,   // EventSourceType
+    0x20,   // EventSeverity
+    0xFF,   // SensorDevice
+    0xFF,   // SensorNumber
+    0x04,   // Entity
+    0x00,   // EntityInstance
+    0x40,   // EventData1
+    0x03    // EventData2
+  },
+  (EFI_PERIPHERAL_FIXED_MEDIA | EFI_P_EC_NOT_DETECTED),
+  L"ASF - ERROR - HDD Failure"
+};
+
+ 
+
+ASF_MESSAGE mAsfProgressMsgList[] = {
+  mAsfMsgHddInit,
+  mAsfMsgApInit,
+  mAsfMsgUserInitSetup,
+  mAsfMsgUsbResouceConfig,
+  mAsfMsgPciResourceConfig,
+  mAsfMsgVideoInit,
+  mAsfMsgKbcInit,
+  mAsfMsgKbcTest,
+  mAsfMsgKbcTest,
+  mAsfMsgOsBoot,
+  mAsfMsgMotherBoardInit
+};
+
+ASF_MESSAGE mAsfErrorMsgList[] = {
+  mAsfMsgNoVideo,
+  mAsfMsgKbdFailure,
+  mAsfMsgHddFailure,
+  mAsfMsgChassisIntrusion,
+  mAsfMsgUserPasswordInvalid,
+  mAsfMsgNoBootMedia
+};
+
+
+EFI_STATUS
+AsfPushMessage (
+  IN UINT8        *Msg,
+  IN UINT32       Size,
+  IN UINT16       *MsgStr    OPTIONAL
+);
+
+#endif
